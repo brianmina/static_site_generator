980e0db45b8b4b783731677516202ee2cba2c579
brianmina <brian.mina17@gmail.com>
2024-05-16 15:25:53 -0400

    adding unit test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,22 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
+    
+    def test_url_none(self):
+        node = TextNode("This is a text node", "bold", url=None)
+        node2 = TextNode("This is a text node", "bold", url=None)
+        self.assertEqual(node, node2)
+    
+    def test_different_type(self):
+        node1 = TextNode("Text node", "bold")
+        node2 = TextNode("Text node", "italic")
+        self.assertNotEqual(node1, node2)
+    
+    def test_different_text(self):
+        node1 = TextNode("Text node1", "bold")
+        node2 = TextNode("Text node2", "bold")
+        self.assertNotEqual(node1, node2)
+    
 
 
 if __name__ == "__main__":
