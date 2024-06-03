69198e827d59819983355cf9fd4a24c1866e4c38
brianmina <brian.mina17@gmail.com>
2024-06-03 08:20:18 -0400

    creation od some content plus  generationg the page

--- src/main.py
+++ src/main.py
@@ -1,35 +1,29 @@
-from textnode import TextNode
 import os
 import shutil
 
-def copy_static(source, destination):
-    # Check if the source directory exists
-    if not os.path.exists(source):
-        print(f"Current working directory: {os.getcwd()}")
-        print(f"The source directory {source} does not exist.")
-        return
-    
-    # Ensure the destination directory exists or create it
-    if not os.path.exists(destination):
-        os.mkdir(destination)
-        print(f"Created the destination directory {destination}")
+from copystatic import copy_files_recursive
+from html_generator import generate_page
 
-    # List the contents of the source directory
-    items = os.listdir(source)
-    print(f"Items to copy from {source}: {items}")
-
-    # Process each item in the source directory
-    for item in items:
-        source_item = os.path.join(source, item)
-        destination_item = os.path.join(destination, item)
-
-        if os.path.isfile(source_item):
-            print(f"Copying file: {source_item} to {destination_item}")
-            shutil.copy(source_item, destination_item)
-        elif os.path.isdir(source_item):
-            print(f"Entering directory: {source_item}")
-            copy_static(source_item, destination_item)
+dir_path_static = 'github.com/brianmina/static_site_generator/static'
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
-if __name__ == "__main__":
-    copy_static('static', 'public')
+def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
+
+
+main()
